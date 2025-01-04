748e88da3474fa12551d51919c7fafefa4a73af1
AStrayker <astrajker@gmail.com>
2025-01-04 14:11:48 +0200

    Update client.py

--- app/tiktok/client.py
+++ app/tiktok/client.py
@@ -36,21 +36,19 @@ class AsyncTikTokClient(httpx.AsyncClient):
 
         soup = BeautifulSoup(page.text, "html.parser")
 
-        # Изменение: поиск скрипта с данными
-        script = soup.find('script', {'id': '__NEXT_DATA__'})
-        if not script:
+        if script := soup.select_one('script[id="__UNIVERSAL_DATA_FOR_REHYDRATION__"]'):
+            script = json.loads(script.text)
+        else:
             raise NoScriptError
 
         try:
-            # Изменение: извлечение данных из скрипта
-            data = json.loads(script.string)
-            video_info = data['props']['pageProps']['itemInfo']['itemStruct']
+            data = script["__DEFAULT_SCOPE__"]["webapp.video-detail"]["itemInfo"]["itemStruct"]
         except KeyError as ex:
             raise NoDataError from ex
 
-        if video_info["id"] != page_id:
+        if data["id"] != page_id:
             raise DifferentPageError
-        return ItemStruct.parse(video_info)
+        return ItemStruct.parse(data)
 
     async def get_video(self, url: str) -> bytes | None:
         resp = await self.get(url)

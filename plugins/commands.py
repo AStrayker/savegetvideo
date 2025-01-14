bf97544c8a53ddb4d9fa2e53d58b1243eae76718
AStrayker <astrajker@gmail.com>
2025-01-14 15:04:11 +0200

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -3,27 +3,53 @@ import bs4, requests
 from bot import DUMP_GROUP
 from apscheduler.schedulers.background import BackgroundScheduler
 from sys import executable
-from os import sys , execl , environ 
-# if you are using service like heroku after restart it changes ip which avoid Ip Blocking Also Restart When Unknown Error occurred and bot is idle 
+from os import sys, execl, environ
+
+# if you are using service like heroku after restart it changes ip which avoid Ip Blocking 
+# Also Restart When Unknown Error occurred and bot is idle 
 RESTART_ON = environ.get('RESTART_ON')
+
+
 def restart():
-     execl(executable, executable, "bot.py")
+    execl(executable, executable, "bot.py")
+
+
 if RESTART_ON:
-   scheduler = BackgroundScheduler()
-   scheduler.add_job(restart, "interval", hours=6)
-   scheduler.start()
-@Mbot.on_message(filters.incoming & filters.private,group=-1)
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(restart, "interval", hours=6)
+    scheduler.start()
+
+
+@Mbot.on_message(filters.incoming & filters.private, group=-1)
 async def monitor(Mbot, message):
-           if DUMP_GROUP:
-              await message.forward(DUMP_GROUP)
-          
+    if DUMP_GROUP:
+        await message.forward(DUMP_GROUP)
+
+
 @Mbot.on_message(filters.command("start") & filters.incoming)
 async def start(Mbot, message):
-          await message.reply(f"Hello 👋👋 {message.from_user.mention()}\n I am A Simple Telegram Bot Can Download From Multiple Social Media Currently Support Instagram ,TikTok, Twitter, Facebook , YouTube(Music and shorts) And So On....! ")
-          
+    await message.reply(
+        f"Hello 👋👋 {message.from_user.mention()}\n"
+        f"I am A Simple Telegram Bot Can Download From Multiple Social Media "
+        f"Currently Support Instagram ,TikTok, Twitter, Facebook , YouTube(Music and shorts) And So On....!"
+    )
+
+
 @Mbot.on_message(filters.command("help") & filters.incoming)
 async def help(Mbot, message):
-          await message.reply("This is user friendly bot so you can simple send your Instagram reel and post links here:) \n eg: `https://www.instagram.com/reel/CZqWDGODoov/?igshid=MzRlODBiNWFlZA==`\n `post:` `https://www.instagram.com/reel/CuCTtORJbDj/?igshid=MzRlODBiNWFlZA==`")
-@Mbot.on_message(filters.command("donate") & filters.command("Donate") & filters.incoming )
+    await message.reply(
+        "This is user friendly bot so you can simple send your Instagram reel and post links here:) \n"
+        "eg: `https://www.instagram.com/reel/CZqWDGODoov/?igshid=MzRlODBiNWFlZA==`\n"
+        "`post:` `https://www.instagram.com/reel/CuCTtORJbDj/?igshid=MzRlODBiNWFlZA==`"
+    )
+
+
+@Mbot.on_message(
+    (filters.command("donate") | filters.command("Donate")) & filters.incoming
+)
 async def donate(_, message):
-       await message.reply_text(f"Donate 🍪 **$** https://www.buymeacoffee.com/Masterolic \n**UPI**`arunrnadh2002@okhdfcbank` \nhttps://www.paypal.me/MasterolicOfficial")
+    await message.reply_text(
+        f"Donate 🍪 **$** https://www.buymeacoffee.com/Masterolic \n"
+        f"**UPI**`arunrnadh2002@okhdfcbank` \n"
+        f"https://www.paypal.me/MasterolicOfficial"
+    )

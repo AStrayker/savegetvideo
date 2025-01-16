4b61fb7998d09195446541642371014966da2091
AStrayker <astrajker@gmail.com>
2025-01-16 14:37:39 +0200

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,68 +1,53 @@
-from pyrogram import filters, Client as Mbot
-import sqlite3
-import os
-from bot import DUMP_GROUP
-from apscheduler.schedulers.background import BackgroundScheduler
-from sys import executable
-from os import execl, environ
+from aiogram import Bot, Dispatcher, F
+from aiogram.types import BufferedInputFile, Message
+from settings import settings
+from tiktok.api import TikTokAPI
+import time
 
-# Если вы используете сервис вроде Heroku, после перезапуска меняется IP, что помогает избежать блокировки IP. Также перезапуск при неизвестной ошибке и когда бот простаивает.
-RESTART_ON = environ.get('RESTART_ON')
+dp = Dispatcher()
 
-def restart():
-    execl(executable, executable, "bot.py")
+filters = [
+    F.text.contains("tiktok.com"),
+    (not settings.allowed_ids)
+    | F.chat.id.in_(settings.allowed_ids)
+    | F.from_user.id.in_(settings.allowed_ids),
+]
 
-if RESTART_ON:
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(restart, "interval", hours=6)
-    scheduler.start()
+# Словарь для отслеживания последнего времени отправки сообщения для каждого пользователя
+last_message_time = {}
 
-# Функция для работы с базой данных
-def init_db():
-    conn = sqlite3.connect('bot.db')
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS greeted_users
-                      (user_id INTEGER PRIMARY KEY)''')
-    conn.commit()
-    conn.close()
+@dp.message(*filters)
+@dp.channel_post(*filters)
+async def handle_tiktok_request(message: Message, bot: Bot) -> None:
+    user_id = message.from_user.id if message.from_user else message.chat.id
+    current_time = time.time()
+    
+    # Проверяем, не отправляли ли мы сообщение этому пользователю недавно
+    if user_id in last_message_time and (current_time - last_message_time[user_id]) < 10:  # 10 секунд таймаут
+        return  # Если да, то пропускаем обработку
+    
+    entries = [
+        message.text[e.offset : e.offset + e.length]
+        for e in message.entities or []
+        if message.text is not None
+    ]
 
-def user_greeted(user_id):
-    conn = sqlite3.connect('bot.db')
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM greeted_users WHERE user_id=?", (user_id,))
-    result = cursor.fetchone()
-    conn.close()
-    return result is not None
+    urls = [
+        u if u.startswith("http") else f"https://{u}"
+        for u in filter(lambda e: "tiktok.com" in e, entries)
+    ]
 
-def mark_user_as_greeted(user_id):
-    conn = sqlite3.connect('bot.db')
-    cursor = conn.cursor()
-    cursor.execute("INSERT OR IGNORE INTO greeted_users (user_id) VALUES (?)", (user_id,))
-    conn.commit()
-    conn.close()
+    async for tiktok in TikTokAPI.download_tiktoks(urls):
+        if not tiktok.video:
+            continue
 
-# Инициализация базы данных при старте бота
-init_db()
+        video = BufferedInputFile(tiktok.video, filename="video.mp4")
+        caption = tiktok.caption if settings.with_captions else None
 
-@Mbot.on_message(filters.incoming & filters.private, group=-1)
-async def monitor(Mbot, message):
-    if DUMP_GROUP:
-        await message.forward(DUMP_GROUP)
-
-@Mbot.on_message(filters.command("start") & filters.incoming)
-async def start(Mbot, message):
-    user_id = message.from_user.id
-    if not user_greeted(user_id):
-        await message.reply(f"Hello 👋👋 {message.from_user.mention()}\nI am A Simple Telegram Bot Can Download From Multiple Social Media Currently Support Instagram, TikTok, Twitter, Facebook, YouTube(Music and shorts) And So On....!")
-        mark_user_as_greeted(user_id)
-    else:
-        # Если пользователь уже был приветствован, просто игнорируем команду
-        pass
-
-@Mbot.on_message(filters.command("help") & filters.incoming)
-async def help(Mbot, message):
-    await message.reply("This is user friendly bot so you can simple send your Instagram reel and post links here:) \n eg: `https://www.instagram.com/reel/CZqWDGODoov/?igshid=MzRlODBiNWFlZA==`\n `post:` `https://www.instagram.com/reel/CuCTtORJbDj/?igshid=MzRlODBiNWFlZA==`")
-
-@Mbot.on_message(filters.command("donate") & filters.command("Donate") & filters.incoming)
-async def donate(_, message):
-    await message.reply_text(f"Donate 🍪 **$** https://www.buymeacoffee.com/Masterolic \n**UPI** `arunrnadh2002@okhdfcbank` \nhttps://www.paypal.me/MasterolicOfficial")
+        if settings.reply_to_message:
+            await message.reply_video(video=video, caption=caption)
+        else:
+            await bot.send_video(chat_id=message.chat.id, video=video, caption=caption)
+    
+    # Отмечаем время отправки сообщения
+    last_message_time[user_id] = current_time

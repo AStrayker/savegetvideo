a5123db2f38aab80b8d4bae24d069e121e356300
AStrayker <astrajker@gmail.com>
2025-01-24 14:19:31 +0200

    Update twitter.py

--- plugins/twitter.py
+++ plugins/twitter.py
@@ -14,15 +14,42 @@ async def twitter_handler(Mbot, message):
         m = await message.reply_sticker("CAACAgIAAxkBATWhF2Qz1Y-FKIKqlw88oYgN8N82FtC8AAJnAAPb234AAT3fFO9hR5GfHgQ")
         try:
             dump_file = await message.reply_video(link, caption="Thank you for using - @InstaReelsdownbot")
-            if DUMP_GROUP:
-                await dump_file.copy(DUMP_GROUP)
         except Exception as e:
-            snd_message = await message.reply(link)
-            await asyncio.sleep(1)
+            print(e)
             try:
+                snd_message = await message.reply(link)
+                await asyncio.sleep(1)
                 dump_file = await message.reply_video(link, caption="Thank you for using - @InstaReelsdownbot")
                 await snd_message.delete()
-                if DUMP_GROUP:
-                    await dump_file.copy(DUMP_GROUP)
             except Exception as e:
-                await snd_message.delete
+                print(e)
+                await snd_message.delete()
+                get_api = requests.get(link).text
+                soup = bs4.BeautifulSoup(get_api, "html.parser")
+                meta_tag = soup.find("meta", attrs={"property": "og:video"})
+                if not meta_tag:
+                    meta_tag = soup.find("meta", attrs={"property": "og:image"})
+                content_value = meta_tag['content']
+                try:
+                    dump_file = await message.reply_video(content_value, caption="Thank you for using - @InstaReelsdownbot")
+                except Exception as e:
+                    print(e)
+                    try:
+                        snd_msg = await message.reply(content_value)
+                        await asyncio.sleep(1)
+                        await message.reply_video(content_value, caption="Thank you for using - @InstaReelsdownbot")
+                        await snd_msg.delete()
+                    except Exception as e:
+                        print(e)
+                        await message.reply("Oops Invalid link or Media Is Not Available:)")
+    except Exception as e:
+        print(e)
+        if LOG_GROUP:
+            await Mbot.send_message(LOG_GROUP, f"{e} {message.chat.id}")
+            await Mbot.send_message(LOG_GROUP, traceback.format_exc())
+    finally:
+        if DUMP_GROUP:
+            if "dump_file" in locals():
+                await dump_file.copy(DUMP_GROUP)
+        await m.delete()
+        await message.reply("Check out @spotify_downloa_bot(music)  @spotifynewss(Channel) \n Please Support Us By /donate To Maintain This Project")

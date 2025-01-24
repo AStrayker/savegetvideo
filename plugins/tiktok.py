520dd9cfc8949fced4255809fc9443f73803d4d0
AStrayker <astrajker@gmail.com>
2025-01-24 14:20:08 +0200

    Update tiktok.py

--- plugins/tiktok.py
+++ plugins/tiktok.py
@@ -10,22 +10,23 @@ async def link_handler(Mbot, message):
         m = await message.reply_sticker("CAACAgIAAxkBATWhF2Qz1Y-FKIKqlw88oYgN8N82FtC8AAJnAAPb234AAT3fFO9hR5GfHgQ")
         get_api = requests.post("https://lovetik.com/api/ajax/search", data={"query": link}).json()
         if get_api['status'] and "Invalid TikTok video url" in get_api['mess']:
-            return await message.reply("Oops Invalid TikTok video url. Please try again :) ")
+            return await message.reply("Oops Invalid TikTok video url. Please try again :)")
         if get_api.get('links'):
             try:
                 if "MP3" in get_api['links'][0]['t']:
-                    await message.reply_photo(get_api['cover'])
+                    try:
+                        await message.reply_photo(get_api['cover'])
+                    except:
+                        pass
                 dump_file = await message.reply_video(get_api['links'][0]['a'], caption="Thank you for using - @InstaReelsdownbot")
-                if DUMP_GROUP:
-                    await dump_file.copy(DUMP_GROUP)
+            except KeyError:
+                return await message.reply("Invalid TikTok video url. Please try again.")
             except Exception:
                 snd_msg = await message.reply(get_api['links'][0]['a'])
                 await asyncio.sleep(1)
                 try:
                     dump_file = await message.reply_video(get_api['links'][0]['a'], caption="Thank you for using - @InstaReelsdownbot")
                     await snd_msg.delete()
-                    if DUMP_GROUP:
-                        await dump_file.copy(DUMP_GROUP)
                 except Exception:
                     pass
     except Exception as e:
@@ -33,5 +34,8 @@ async def link_handler(Mbot, message):
             await Mbot.send_message(LOG_GROUP, f"TikTok {e} {link}")
             await Mbot.send_message(LOG_GROUP, traceback.format_exc())
     finally:
-        await m.delete()
+        if 'dump_file' in locals():
+            if DUMP_GROUP:
+                await dump_file.copy(DUMP_GROUP)
+            await m.delete()
         await message.reply("Check out @spotify_downloa_bot(music)  @spotifynewss(Channel) \n Please Support Us By /donate To Maintain This Project")
